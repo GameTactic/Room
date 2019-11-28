08e64b01e34d52378bc087edecad3a5b1573579f
Nathanael Couret <nathanael.couret@gmail.com>
2019-11-28 13:35:41 +0100

    Add SideToolbar component with simple flex layout to Room view

---
+++ src/components/toolbars/SideToolbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-btn-toggle class="flex-column side-toolbar">
+      <slot></slot>
+    </v-btn-toggle>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+  @Component({
+    name: 'side-toolbar'
+  })
+export default class SideToolbar extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+.side-toolbar {
+  padding: 0.5ch;
+  margin: 1ch;
+}
+</style>
