9d673d7fb5cb78a52dc79014cfd27ed27cf6c14d
Sam Bryan <bryan.samuel1@hotmail.co.uk>
2020-01-04 18:22:14 +0100

    move from laptop back to pc

---
+++ src/components/TheToolPanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn-toggle class="flex-column btn-toggle-box">
+    <tool-container :id="id" icon="fa-map-pin" toolname="ping" :popout="false" />
+    <tool-container :id="id" icon="fa-pen" toolname="freedraw" :popout="true"><freedraw-template /></tool-container>
+  </v-btn-toggle>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import ToolContainer from './canvas-tools/ToolContainer.vue'
+import FreedrawTemplate from './canvas-tools/templates/Freedraw.vue'
+import { ITool } from '../types/canvas'
+
+@Component({
+  name: 'TheToolPanel',
+  components: {
+    ToolContainer,
+    FreedrawTemplate
+  }
+})
+export default class TheToolPanel extends Vue {
+  @Prop() private id!: string;
+
+  tools: ITool[] = []
+}
+</script>
+<style scoped lang="scss">
+.btn-toggle-box{
+  margin: 0.25rem;
+  position: fixed;
+}
+</style>
