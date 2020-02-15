471929db360b5770e7c7a3c634bf98eeb9cae77a
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2020-02-15 22:33:32 +0100

    Fixed freedraw

--- src/store/modules/tools.ts
+++ src/store/modules/tools.ts
@@ -2,7 +2,6 @@ import { ActionContext, Module } from 'vuex'
 import { Tool } from '@/tools/Tool'
 import Ping from '@/tools/Ping'
 import FreeDraw from '@/tools/FreeDraw'
-import Erase from '@/tools/Erase'
 
 export enum ToolGetters {
   ACTIVE_TOOL = 'activeTool',
@@ -49,8 +48,7 @@ const ToolModule: Module<ToolState, {}> = {
       enabled: true,
       tools: [
         new Ping('ping', 5, '#005555'),
-        new FreeDraw('freedraw', 2, '#FF0000'),
-        new Erase('erase', 2, '#FFFFFF')
+        new FreeDraw('freedraw', 2, '#FF0000')
       ]
     }
   },
