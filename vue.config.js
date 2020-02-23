66ae36ebdd6d10cf13dd687aa0236d830cac866e
Niko Granö <niko@ironlions.fi>
2020-02-23 22:05:41 +0200

    Move SCSS.

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,11 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "@/styles/_variables.scss";`
+        prependData: '@import "@/assets/scss/_variables.scss";'
       }
     }
   },
-  transpileDependencies: ['vuetify']
+
+  transpileDependencies: ['vuetify'],
+  productionSourceMap: false
 }
