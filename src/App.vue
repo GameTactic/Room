a828e9523392d9aa878fdac07661f7843532a130
samuel_bryan <bryan.samuel1@hotmail.co.uk>
2020-03-01 15:32:49 +0000

    93 - Set up room state

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,24 @@
   </v-app>
 </template>
 
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+import { Action } from 'vuex-class'
+import { RoomAction, Game, Locale } from '@/store/modules/room'
+
+@Component({
+  name: 'TheApp'
+})
+export default class TheApp extends Vue {
+    @Action(`room/${RoomAction.SET_GAME}`) setGame!: (game: Game) => void
+    @Action(`room/${RoomAction.SET_LOCALE}`) setLocale!: (locale: Locale) => void
+    created () {
+      this.setGame(Game['WOT'])
+      this.setLocale(Locale['ENUK'])
+    }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
