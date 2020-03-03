7fbba83e59d3d577cd30615755d0d06c42009ee1
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2020-03-03 19:14:09 +0100

    Commented calcTBar function

--- src/tools/shapes/Line.ts
+++ src/tools/shapes/Line.ts
@@ -67,28 +67,40 @@ export default class Line {
   }
 
   calcTBar = (x1: number, y1: number, x2: number, y2: number): number[] => {
+    // Checking for empty fields
     if (x2 === null || y2 === null || x2 === undefined || y2 === undefined) {
+      // If empty fields, return [0, 0] (Line will not be drawn)
       return [0, 0]
     } else {
+      // The length of one of the lines in the T bar. Full length of T bar will be twice the distance
       const distance = 15
-      const f = (-1 * (x2 - x1))
-      const d = (-1 * (y2 - y1))
-      if (f === 0 || d === 0) {
+      // Finding x and y vectors and make them point the right way
+      const xVector = (-1 * (x2 - x1))
+      const yVector = (-1 * (y2 - y1))
+      // One vector cant be 0, return [0, 0] if thats the case (Line will not be drawn)
+      if (xVector === 0 || yVector === 0) {
         return [0, 0]
       } else {
+        // Defines vector AB
         let AB = []
-        if (Math.abs(f) < Math.abs(d)) {
-          AB = [f / d, 1]
+        // Factorizing x and y
+        if (Math.abs(xVector) < Math.abs(yVector)) {
+          AB = [xVector / yVector, 1]
         } else {
-          AB = [1, d / f]
+          AB = [1, yVector / xVector]
         }
+        // Inversing the vector
         const ABInv = [AB[1] * -1, AB[0]]
+        // Finding the length of the T bar. It will most likely be >distance
         const length = Math.sqrt(Math.pow(ABInv[0] * distance, 2) + Math.pow(ABInv[1] * distance, 2))
+        // Offset variable makes sure the length of the T bar dont vary too much
         const offset = distance - ((length - distance) / Math.sqrt(2))
+        // Calculating points where T bar should be drawn
         const xx1 = x2 + ABInv[0] * offset
         const yy1 = y2 + ABInv[1] * offset
         const xx2 = x2 + ABInv[0] * -offset
         const yy2 = y2 + ABInv[1] * -offset
+        // Returning points for T bar
         return [xx1, yy1, xx2, yy2]
       }
     }
