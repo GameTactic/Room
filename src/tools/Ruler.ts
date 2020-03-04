9b84d69b3e494566091e9b42d1b841e441e3386f
Samuel Bryan <bryan.samuel1@hotmail.co.uk>
2020-03-04 20:20:04 +0000

    Rewrite on tools to use new structure (#112)
    * Added ruler
    
    * Removed ruler template
    
    * Added interface for returntypes
    
    * Added interface for returntypes
    
    * Cleaned up canvasElement and fixed minor bugs
    
    * Rewrite circle to use new structure
    
    * Rewrote freedraw to use new structure
    
    * Rewrite ping to use new structure
    
    * Small cleanup

--- src/tools/Ruler.ts
+++ src/tools/Ruler.ts
@@ -3,43 +3,59 @@ import Konva from 'konva'
 import { CanvasElement } from '@/types/Canvas'
 import uuid from 'uuid'
 import throttle from 'lodash.throttle'
-import FreedrawCreator from '@/tools/shapes/FreedrawCreator'
+import RulerCreator from '@/tools/shapes/RulerCreator'
 
-export default class FreeDraw implements Tool {
-  private freedraw: Konva.Line;
-  private freedrawCreator: FreedrawCreator;
+export default class Ruler implements Tool {
+  private line: Konva.Line
+  private text: Konva.Text
+  private circle: Konva.Circle
+  private rulerCreator: RulerCreator
   constructor (public readonly name: string,
                public size: number,
-               public colour: string) {
-    this.freedraw = new Konva.Line()
-    this.freedrawCreator = new FreedrawCreator()
+               public colour: string,
+               public temporary: boolean) {
+    this.line = new Konva.Line()
+    this.text = new Konva.Text()
+    this.circle = new Konva.Circle()
+    this.rulerCreator = new RulerCreator(this.size, this.colour)
   }
 
   // eslint-disable-next-line
   mouseDownAction = (e: Konva.KonvaPointerEvent, canvasElement: CanvasElement, layer: Konva.Layer, _socket: WebSocket): void => {
     canvasElement.data = [e.evt.x, e.evt.y]
     canvasElement.id = uuid()
-    this.freedrawCreator = new FreedrawCreator(this.size, this.colour)
-    this.freedraw = this.freedrawCreator.create(canvasElement, layer)
+    const result = this.rulerCreator.create(canvasElement, layer)
+    this.circle = result.circle
+    this.line = result.line
+    this.text = result.text
   }
 
   // eslint-disable-next-line
   mouseMoveAction = throttle((e: Konva.KonvaPointerEvent, canvasElement: CanvasElement, layer: Konva.Layer, socket: WebSocket): void => {
-    canvasElement.data = canvasElement.data.concat([e.evt.x, e.evt.y])
-    this.freedrawCreator.move(canvasElement, layer, this.freedraw)
+    const pos = {
+      x: e.evt.x,
+      y: e.evt.y
+    }
+    this.rulerCreator.move(canvasElement, layer, pos, this.line, this.text, this.circle)
     layer.batchDraw()
-  }, 0)
+  }, 5)
 
   mouseUpAction = (e: Konva.KonvaPointerEvent, canvasElement: CanvasElement, _layer: Konva.Layer, socket: WebSocket): void => {
-    this.sendToWebsockets(socket, canvasElement)
+    canvasElement.data = canvasElement.data.concat([e.evt.x, e.evt.y])
+    socket.send(JSON.stringify(canvasElement))
   }
 
   renderCanvas = (canvasElement: CanvasElement, layer: Konva.Layer): void => {
-    this.freedrawCreator = new FreedrawCreator(
+    this.rulerCreator = new RulerCreator(
       canvasElement.tool.size || this.size,
       canvasElement.tool.colour || this.colour
     )
-    this.freedrawCreator.create(canvasElement, layer)
+    const result = this.rulerCreator.create(canvasElement, layer)
+    const pos = {
+      x: canvasElement.data[2],
+      y: canvasElement.data[3]
+    }
+    this.rulerCreator.move(canvasElement, layer, pos, result.line, result.text, result.circle)
     layer.batchDraw()
   }
 
@@ -49,11 +65,11 @@ export default class FreeDraw implements Tool {
       id: canvasElement.id,
       layerId: canvasElement.layerId,
       tool: {
-        name: 'freedraw',
+        name: 'ruler',
         colour: this.colour,
         size: this.size
       },
-      temporary: false,
+      temporary: this.temporary,
       data: canvasElement.data
     }
     socket.send(JSON.stringify(data))
