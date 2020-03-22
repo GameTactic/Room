f3fe7e1de1bb4316dea5befa0dc5cfcccac56973
Nathanael Couret <nathanael.couret@gmail.com>
2020-03-22 11:58:24 +0100

    #15 - Refactored navbar component

---
+++ src/components/navigation/TheNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <the-nav-extra-large class="the-nav-extra-large" :id="id"/>
+    <the-nav-small class="the-nav-small" :id="id"/>
+    <the-nav-extra-small class="the-nav-extra-small" :id="id"/>
+  </div>
+</template>
+
+<script lang="ts">
+import TheNavExtraLarge from '@/components/navigation/TheNavExtraLarge.vue'
+import TheNavSmall from '@/components/navigation/TheNavSmall.vue'
+import TheNavExtraSmall from '@/components/navigation/TheNavExtraSmall.vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+  @Component({
+    name: 'the-nav',
+    components: {
+      TheNavExtraLarge,
+      TheNavSmall,
+      TheNavExtraSmall
+    }
+  })
+export default class extends Vue {
+    @Prop() id!: string
+}
+</script>
+<style scoped lang="scss">
+
+  @media (max-width: 575px) {
+    .the-nav-extra-large, .the-nav-small {
+      display: none;
+    }
+  }
+
+  @media (min-width: 576px) and (max-width: 1199px) {
+    .the-nav-extra-large, .the-nav-extra-small {
+      display: none;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .the-nav-small, .the-nav-extra-small {
+      display: none;
+    }
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <the-nav :id="id" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import TheNav from '@/components/navigation/TheNav.vue'
+
+  @Component({
+    name: 'Login',
+    components: {
+      TheNav
+    }
+  })
+export default class extends Vue {
+    @Prop() id!: string
+}
+</script>
+
+<style scoped>
+
+</style>
