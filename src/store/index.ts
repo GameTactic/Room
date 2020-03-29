9544b20d5be01c6a41207ef08f85b49e4aae7f69
Samuel Bryan <bryan.samuel1@hotmail.co.uk>
2020-03-29 20:42:38 +0100

    142 - Wows Search using signed commit (#158)
    Signed-off-by: samuel_bryan <bryan.samuel1@hotmail.co.uk>

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,7 @@ import ToolModule from '@/store/modules/tools'
 import CursorModule from '@/store/modules/cursor'
 import SocketModule from '@/store/modules/socket'
 import CanvasModule from '@/store/modules/canvas'
-import RoomModule from '@/store/modules/room'
+import RoomModule, { RoomState } from '@/store/modules/room'
 import AuthenticationModule from '@/store/modules/authentication'
 
 Vue.use(Vuex)
@@ -18,7 +18,7 @@ export enum Namespaces {
   AUTH = 'authentication'
 }
 
-export default new Vuex.Store<{}>({
+export default new Vuex.Store<any>({
   modules: {
     [Namespaces.TOOLS]: ToolModule,
     [Namespaces.CURSOR]: CursorModule,
