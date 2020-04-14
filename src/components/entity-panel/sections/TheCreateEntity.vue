b4169ab4ff0b6e3a17d0ca17fa429e13f7ad23d4
samuel_bryan <bryan.samuel1@hotmail.co.uk>
2020-04-14 21:06:54 +0100

    Add New Entity - Entity Panel

--- src/components/entity-panel/sections/TheCreateEntity.vue
+++ src/components/entity-panel/sections/TheCreateEntity.vue
@@ -1,31 +1,195 @@
 <template>
-  <div>
-    <v-autocomplete
-      :items="shipsData"
-      :search-input.sync="search"
-      class="body-1 custom-autocomplete"
-      color="primary"
-      hide-no-data
-      hide-selected
-      label="Search for your ship"
-      placeholder="Start typing to Search"
-      prepend-icon="fa-search"
-      autocomplete="new-password"
-      return-object
-      @change="autoCompleteOnChangeHandler"
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    left
+    transition="slide-x-reverse-transition"
+    content-class="custom-menu-content-class"
+    absolute
+    :nudge-left="10"
+    :nudge-top="10"
+    offset-x
+    @update:return-value="menuOnMinimiseHandler"
+  >
+    <template v-slot:activator="{ on: showEntityFields }">
+      <v-chip-group
+        v-on="showEntityFields"
+        mandatory
+        light
+        class="custom-chip-group"
+        :style="`border-bottom: 2px solid ${chipColour}`"
+      >
+        <v-chip
+          v-for="team in teams"
+          :background-color="team.color"
+          active-class="custom-chip-active-class"
+          filter
+          label
+          outlined
+          :key="`chip-${team.id}`"
+          @click="chipOnClickHandler(team.color)"
+        >
+          <v-icon :color="team.color">fa-users</v-icon>
+        </v-chip>
+      </v-chip-group>
+      <v-container class="pa-1">
+        <v-row
+          v-if="entityData.length"
+          class="custom-entity-card-row"
+        >
+          <v-col align="center" :cols="12" class="pb-1">
+            <v-autocomplete
+              :items="entityData"
+              :search-input.sync="search"
+              class="body-1"
+              color="primary"
+              hide-no-data
+              dense
+              eager
+              clearable
+              clear-icon="fa-times"
+              cache-items
+              hide-selected
+              label="Search for your ship"
+              placeholder="Start typing to Search"
+              prepend-icon="fa-search"
+              autocomplete="new-password"
+              return-object
+              @change="autoCompleteOnChangeHandler"
+            >
+              <template v-slot:item="data">
+                <v-list-item-avatar size="29" class="custom-list-item-avatar">
+                  <img :src="data.item.image">
+                </v-list-item-avatar>
+                <v-list-item-content class="custom-list-item-content">
+                  <v-list-item-title v-text="data.item.text"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+            </v-autocomplete>
+          </v-col>
+          <v-col
+            v-for="entity in updatedEntities"
+            :key="entity.value"
+            align-self="center"
+            :cols="4"
+            class="px-2 pb-2"
+          >
+            <v-badge
+              overlap
+              color="transparent"
+              offset-x="30"
+              :value="entityCardHover === entity.value"
+            >
+              <template v-slot:badge>
+                <v-menu offset-y nudge-left="20">
+                  <template v-slot:activator="{ on: menuItem }">
+                    <v-btn
+                      icon
+                      ripple
+                      small
+                      v-on="menuItem"
+                      @mouseover="entityCardOnMouseHoverHandler(entity.value)"
+                    >
+                      <v-icon small>fa-ellipsis-v</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list dense>
+                    <v-list-item
+                      v-for="(item, index) in cardMenuItems"
+                      :key="index"
+                      class="custom-entity-card-list-item"
+                      @click="cardMenuOnClickHandler(entity, item.action)"
+                    >
+                      <v-icon
+                        small
+                        :color="item.action === 'delete' ? 'primary' : ''"
+                        :title="item.title"
+                        v-text="item.icon"
+                      />
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </template>
+              <v-card
+                v-if="entityData.length"
+                class="custom-entity-card"
+                tile
+                draggable
+                @mouseover="entityCardOnMouseHoverHandler(entity.value)"
+              >
+                <v-badge
+                  overlap
+                  left
+                  color="transparent"
+                  offset-x="30"
+                  :value="entityCardHover === entity.value"
+                >
+                  <template v-slot:badge>
+                    <v-btn
+                      icon
+                      v-on="showEntityFields"
+                      :disabled="isEntityPropertiesShown"
+                      ripple
+                      small
+                      @click="entityCardOnEditHandler(entity)"
+                    >
+                      <v-icon small>fa-edit</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-card
+                    v-if="entityData.length"
+                    class="custom-entity-card"
+                    raised
+                    tile
+                    draggable
+                    @mouseover="entityCardOnMouseHoverHandler(entity.value)"
+                  >
+                    <v-container class="pa-1">
+                      <v-row class="text-center mx-0">
+                        <v-col cols="12" class="pa-0">
+                          <img class="custom-entity-card-image" width="40" :src="entity.image">
+                        </v-col>
+                        <v-col cols="12" class="pa-0">
+                          <div class="caption">{{entity.shortText}}</div>
+                          <v-chip v-if="entity.tier" x-small pill>T-{{entity.tier}}</v-chip>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card>
+                </v-badge>
+              </v-card>
+            </v-badge>
+          </v-col>
+        </v-row>
+        <v-row v-else>
+          <v-col cols="12">
+            <v-skeleton-loader
+              type="list-item-avatar"
+            />
+          </v-col>
+          <v-col
+            v-for="i in 4"
+            :key="i"
+          >
+            <v-skeleton-loader
+              width="90"
+              height="90"
+              type="card"
+              tile
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+    <v-sheet
+      flat
+      tile
+      elevation="0"
+      class="pa-3"
+      width="200"
     >
-      <template v-slot:item="data">
-        <v-list-item-avatar size="29" class="custom-list-item-avatar">
-          <img :src="data.item.image">
-        </v-list-item-avatar>
-        <v-list-item-content class="custom-list-item-content">
-          <v-list-item-title v-text="data.item.text"></v-list-item-title>
-        </v-list-item-content>
-      </template>
-    </v-autocomplete>
-    <div>
       <div
-        v-for="field in fields"
+        v-for="(field, index) in fields"
         :key="field.id"
       >
         <v-text-field
@@ -35,62 +199,29 @@
           :label="field.label"
           :placeholder="field.placeholder"
           :suffix="field.suffix"
-          :disabled="!search"
-          :color="fieldSavedColour(field)"
           class="custom-text-field"
           outlined
           dense
-          :prepend-inner-icon="determinePrependIcon(fields, field)"
-          @change="onTextFieldChangeHandler(fields, field, $event)"
-          @click:prepend-inner="onViewFieldHandler(fields, field)"
-        >
-          <template v-slot:append>
-            <v-icon
-              class="custom-icon-append-hover"
-              small
-              :color="fieldSavedColour(field)"
-              @click="onSaveFieldHandler(fields, savedFields, field)"
-            >
-              fa-save
-            </v-icon>
-          </template>
-        </v-text-field>
+          @change="onTextFieldChangeHandler(entities, selectedEntityId, field, $event)"
+        />
+        <div v-if="index === fields.length -1" class="caption"><b>Note</b> Changes are saved automatically</div>
       </div>
-    </div>
-    <div class="custom-team-controls-container">
-      <v-select
-        color="primary"
-        class="body-2"
-        label="I want to ..."
-        value="team-0"
-        dense
-        :items="entitySelectOptions"
-        return-object
-      />
-      <div class="custom-team-controls">
-        <v-switch
-          dense
-          v-model="addToMapToggle"
-          label="Place on Map"
-        ></v-switch>
-        <v-btn
-          color="primary"
-          :disabled="!search"
-        >
-          Go
-        </v-btn>
-      </div>
-    </div>
-  </div>
+    </v-sheet>
+  </v-menu>
 </template>
 <script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
 import { Prop } from 'vue-property-decorator'
 import Component from 'vue-class-component'
-import axios from 'axios'
-import Games from '@/mixins/Games'
-import { ShipItem, ShipField } from '@/types/Games/Wows'
-import { SavedField } from '@/types/Games/Index.ts'
+import { Item, Field } from '@/types/Games/Index'
 import { MenuItem } from '@/components/TheEntityPanel.vue'
+import Games from '@/mixins/Games'
+import { GameName } from '../../../store/modules/room'
+
+interface RelatedTargetMouseEvent extends MouseEvent {
+  toElement: any;
+}
 
 @Component({
   name: 'TheCreateEntity',
@@ -99,140 +230,88 @@ import { MenuItem } from '@/components/TheEntityPanel.vue'
 export default class TheCreateEntity extends Games {
   @Prop() private clickedItem!: string;
   @Prop() private teams!: MenuItem[];
-  @Prop() private shipsData!: ShipItem[];
+  @Prop() private entityData!: Item[];
+  @Prop() private fields!: Field[];
+  @Prop() private game!: GameName;
 
+  chipColour = 'green'
+  entityCardHover = 0
+  selectedEntityId = -1
   search = ''
-  selectedEntityId = ''
-  addToMapToggle = true
-  savedFields: SavedField[] = []
-  fields: ShipField[] = [{
-    id: 'shipMainBatteryRange',
-    value: '',
-    hide: false,
-    label: 'Main Battery Range',
-    placeholder: 'e.g. 10',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
-  }, {
-    id: 'shipSurfaceConcealment',
-    value: '',
-    hide: false,
-    label: 'Surface Concealment',
-    placeholder: 'e.g. 5',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
-  }, {
-    id: 'shipPlaneConcealment',
-    value: '',
-    hide: false,
-    label: 'Plane Concealment',
-    placeholder: 'e.g. 3',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
-  }, {
-    id: 'shipTorpedoesRange',
-    value: '',
-    hide: false,
-    label: 'Torpedoes Range',
-    placeholder: 'e.g. 10',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
-  }, {
-    id: 'shipRadarRange',
-    value: '',
-    hide: false,
-    label: 'Radar Range',
-    placeholder: 'e.g. 9',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
-  }, {
-    id: 'shipHydroRange',
-    value: '',
-    hide: false,
-    label: 'Hydro Range',
-    placeholder: 'e.g. 5',
-    saved: undefined,
-    suffix: 'km',
-    canvas: {
-      isVisible: false,
-      showIcon: true
-    }
+  menu = false
+  cardMenuItems = [{
+    action: 'delete',
+    title: `Remove ${this.game === 'wows' ? 'Ship' : 'Tank'}`,
+    icon: 'fa-times'
   }]
+  isEntityPropertiesShown = false
+  entities: Item[] = []
 
-  async autoCompleteOnChangeHandler (shipItem: ShipItem) {
-    const response = await axios.get(`https://api.worldofwarships.eu/wows/encyclopedia/shipprofile/?ship_id=${shipItem.value}&application_id=d84a218b4fec37003e799f13777bf880`)
-    const shipData = response.data.data[shipItem.value]
-    if (shipData) {
-      this.selectedEntityId = shipData.ship_id
-      this.updateField(this.fields, shipData, 'shipMainBatteryRange', 'artillery.distance')
-      this.updateField(this.fields, shipData, 'shipTorpedoesRange', 'torpedoes.distance')
-      this.updateField(this.fields, shipData, 'shipSurfaceConcealment', 'concealment.detect_distance_by_ship')
-      this.updateField(this.fields, shipData, 'shipPlaneConcealment', 'concealment.detect_distance_by_plane')
-    } else {
-      this.selectedEntityId = shipItem.value.toString()
-      const foundMainBatteryField = this.findField(this.fields, 'shipMainBatteryRange')
-      const foundTorpedoesField = this.findField(this.fields, 'shipTorpedoesRange')
-      const foundSurfaceConcealmentField = this.findField(this.fields, 'shipSurfaceConcealment')
-      const foundPlaneConcealmentField = this.findField(this.fields, 'shipPlaneConcealment')
-      if (foundMainBatteryField) foundMainBatteryField.value = ''
-      if (foundTorpedoesField) {
-        foundTorpedoesField.value = ''
-        foundTorpedoesField.hide = false
-      }
-      if (foundSurfaceConcealmentField) foundSurfaceConcealmentField.value = ''
-      if (foundPlaneConcealmentField) foundPlaneConcealmentField.value = ''
+  get updatedEntities (): Item[] {
+    return this.entities
+  }
+
+  set updatedEntities (newValue: Item[]) {
+    this.entities = newValue
+  }
+
+  menuOnMinimiseHandler () {
+    this.isEntityPropertiesShown = false
+  }
+
+  cardMenuOnClickHandler (clickedEntity: Item, action: string) {
+    if (action === 'delete') {
+      this.entities = [...this.entities].filter((entity: Item) => entity.value !== clickedEntity.value)
     }
-    const foundRadarField = this.findField(this.fields, 'shipRadarRange')
-    const foundHydroField = this.findField(this.fields, 'shipHydroRange')
-    if (foundRadarField) foundRadarField.value = ''
-    if (foundHydroField) foundHydroField.value = ''
+  }
 
-    const foundSavedFields = this.savedFields.filter((savedField: SavedField) => savedField.entityId === this.selectedEntityId)
-    foundSavedFields.forEach((foundSavedField: SavedField) => {
-      const foundField = this.findField(this.fields, foundSavedField.fieldId)
-      if (foundField) {
-        foundField.value = foundSavedField.value
-        foundField.canvas.isVisible = foundSavedField.canvasIsVisible
-        foundField.canvas.showIcon = foundSavedField.canvasShowIcon
-        foundField.saved = true
-      }
+  entityCardOnEditHandler (clickedEntity: Item) {
+    this.isEntityPropertiesShown = true
+    this.selectedEntityId = clickedEntity.value
+    this.fields.forEach((field: Field) => {
+      this.updateField(this.fields, clickedEntity, field.id, field.path)
     })
   }
 
-  get entitySelectOptions () {
-    return this.teams.map((team: MenuItem, index: number) => ({
-      text: `Add to ${team.title}`,
-      value: `team-${index}`
-    }))
+  entityCardOnMouseHoverHandler (cardValue: number) {
+    this.entityCardHover = cardValue
+  }
+
+  chipOnClickHandler (color: string) {
+    this.$data.chipColour = color
+  }
+
+  async autoCompleteOnChangeHandler (shipItem: Item) {
+    if (this.game === 'wows') {
+      if (shipItem) {
+        this.entities.unshift(shipItem)
+        const response = await axios.get(`https://api.worldofwarships.eu/wows/encyclopedia/shipprofile/?ship_id=${shipItem.value}&application_id=d84a218b4fec37003e799f13777bf880`)
+        const shipData = response.data.data[shipItem.value]
+        const foundNewEntity = this.entities.find((entity: Item) => entity.value === shipItem.value)
+        if (foundNewEntity) {
+          foundNewEntity.data = shipData
+        }
+      }
+    }
   }
 }
 </script>
 <style lang="scss">
-.custom-list-item-content {
-  padding: 0px;
-  >div {
-    font-size: 0.8rem;
+.custom-add-card {
+  display: flex;
+  i {
+    align-items: center;
+    width: 100%;
   }
+}
+
+.custom-chip-group {
+  margin: 0px 0.5rem;
+}
+
+.custom-chip-active-class i:first-child {
+  color: $room-primary !important;
+  font-size: 16px !important;
 }
 
 .custom-list-item-avatar {
@@ -242,33 +321,36 @@ export default class TheCreateEntity extends Games {
   }
 }
 
+.custom-entity-card-row {
+  margin-left: 4px;
+  margin-right: 4px;
+}
+
+.custom-entity-card {
+  min-width: 90px;
+  min-height: 90px;
+  display: flex;
+  align-items: center;
+}
+
+.custom-entity-card-image {
+  background-color: $room-primary;
+  border-radius: 50%;
+  filter: opacity(0.75);
+}
+
+.custom-entity-card-list-item {
+  justify-content: center;
+}
+
+.custom-menu-content-class {
+  border-radius: 0px;
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)
+}
+
 .custom-text-field.custom-text-field {
   input{
     text-align: right;
   }
-  .v-input__append-inner {
-    align-self: normal;
-    margin-top: 0px !important;
-    margin-left: 0.5rem;
-  }
-  button {
-    font-size: 1rem;
-  }
-}
-
-.custom-team-controls-container {
-  border: 1px solid #949494;
-  border-radius: 0.25rem;
-  padding: 20px 10px 0px 10px;
-}
-
-.custom-team-controls {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.custom-icon-append-hover:hover {
-  color: darkseagreen !important;
 }
 </style>
