182639678c8ad96039ca64fd7e4b2437f8b01c6c
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2020-04-18 15:05:13 +0200

    Before merge with BasicEntityDragDrop

--- src/tools/shapes/FreedrawCreator.ts
+++ src/tools/shapes/FreedrawCreator.ts
@@ -29,14 +29,12 @@ export default class FreedrawCreator extends Shape implements FreeDrawCreatorInt
   createFreedrawElement = (canvasElement: CanvasElement, event: CustomEvent | CustomStageEvent, colour?: string, size?: number): Konva.Shape & Konva.Line => {
     return new Konva.Line({
       globalCompositeOperation: 'source-over',
-      lineJoin: 'bevel',
       points: canvasElement.data.map((num, index) => (index % 2) ? this.formatX(num, event) : this.formatY(num, event)),
       stroke: colour || canvasElement.tool.colour || this.colour,
       strokeWidth: size || canvasElement.tool.size || this.size,
-      bezier: true,
-      lineCap: 'round',
       hitStrokeWidth: this.hitStroke,
-      id: canvasElement.id
+      id: canvasElement.id,
+      lineJoin: 'round'
     })
   }
 }
