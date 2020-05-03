eda7c8f6d73b7419d0cb383e9a7dc6518018b639
Giovanni Acciai <mightywizard83@gmail.com>
2020-05-03 11:58:44 +0200

    Implement MVP1 - Fix color issues with tools (#197)

--- src/assets/tool-icons/TBarHead.vue
+++ src/assets/tool-icons/TBarHead.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 391.4 419.11"><title>t-bar-head</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1-2"><path d="M311.4,249.56H24a24,24,0,0,1-24-24v-32a24,24,0,0,1,24-24H311.4"/><path d="M391.4,249.56V406.05c0,7.21-10.75,13.06-24,13.06h-32c-13.25,0-24-5.85-24-13.06V249.56"/><path d="M311.4,249.56V19.23C311.4,8.61,322.15,0,335.4,0h32c13.25,0,24,8.61,24,19.23V249.56"/></g></g><rect x="311.4" y="149" width="80" height="161"/><rect x="191" y="169.56" width="200.4" height="80"/></svg>
+  <svg fill="var(--v-secondary-lighten2)" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 391.4 419.11"><title>t-bar-head</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1-2"><path d="M311.4,249.56H24a24,24,0,0,1-24-24v-32a24,24,0,0,1,24-24H311.4"/><path d="M391.4,249.56V406.05c0,7.21-10.75,13.06-24,13.06h-32c-13.25,0-24-5.85-24-13.06V249.56"/><path d="M311.4,249.56V19.23C311.4,8.61,322.15,0,335.4,0h32c13.25,0,24,8.61,24,19.23V249.56"/></g></g><rect x="311.4" y="149" width="80" height="161"/><rect x="191" y="169.56" width="200.4" height="80"/></svg>
 </template>
 <script>
 export default {
