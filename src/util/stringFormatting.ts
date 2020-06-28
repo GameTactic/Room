50275fc22e1c77814ca16798c117eb605c5c12d1
samuel_bryan <bryan.samuel1@hotmail.co.uk>
2020-06-28 15:25:27 +0100

    Try to fix build

--- src/util/stringFormatting.ts
+++ src/util/stringFormatting.ts
@@ -1,7 +1,7 @@
-export const capitaliseFirstLetter = (sentence: string): string => {
+export function capitaliseFirstLetter (sentence: string): string {
   return sentence.charAt(0).toUpperCase() + sentence.slice(1)
 }
 
-export const camelCaseToPascalCaseWithSpaces = (sentence: string): string => {
+export function camelCaseToPascalCaseWithSpaces (sentence: string): string {
   return capitaliseFirstLetter(sentence.replace(/[A-Z]/g, (capturedString: string) => ` ${capturedString}`))
 }
